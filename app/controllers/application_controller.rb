9510bd27e4c466c6a57de005dc168e63fcb1694b
Bianca M Fernandez <bianca.maria.fernandez.22@gmail.com>
2017-10-17 19:25:20 -0700

    Change application html back to messages and result text in flash, db migration addding reference between works and user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :find_user
-
-  # before_action :require_login
+  before_action :find_user, :require_login
 
   def render_404
     # DPR: supposedly this will actually render a 404 page in production
@@ -16,23 +14,31 @@ class ApplicationController < ActionController::Base
 
     if result
       flash[:status] = :success
-      flash[:message] = "Successfully saved #{model.class} #{model.id}"
+      flash[:result_text] = "Successfully saved #{model.class} #{model.id}"
     else
       flash.now[:status] = :failure
-      flash.now[:message] = "Failed to save #{model.class}"
+      flash.now[:result_text] = "Failed to save #{model.class}"
       flash.now[:details] = model.errors.messages
     end
 
     return result
   end
 
+  def require_login
+    @user = User.find_by(id: session[:user_id])
+    unless @user
+      flash[:status] = :failure
+      flash[:result_text] = "You must be logged in to do that!"
+      redirect_to root_path
+    end
+  end
 
 
   private
   def find_user
     if session[:user_id]
       @login_user = User.find_by(id: session[:user_id])
-      @user = User.find_by(id: session[:user_id])
+      # @user = User.find_by(id: session[:user_id])
     end
   end
 
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-  # skip_before_action :require_login, only: [:login]
+  skip_before_action :require_login, only: [:login, :logout]
 
   def login
 
